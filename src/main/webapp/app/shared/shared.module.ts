a4a3bd184372dce917b03e6e8a208b65afc27d06
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2018-08-11 11:37:24 -0700

    Initial application generated by JHipster-4.14.5

---
+++ src/main/webapp/app/shared/shared.module.ts
@@ -0,0 +1,49 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import {
+    OlddSharedLibsModule,
+    OlddSharedCommonModule,
+    CSRFService,
+    AuthServerProvider,
+    AccountService,
+    UserService,
+    StateStorageService,
+    LoginService,
+    LoginModalService,
+    JhiLoginModalComponent,
+    Principal,
+    HasAnyAuthorityDirective,
+} from './';
+
+@NgModule({
+    imports: [
+        OlddSharedLibsModule,
+        OlddSharedCommonModule
+    ],
+    declarations: [
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective
+    ],
+    providers: [
+        LoginService,
+        LoginModalService,
+        AccountService,
+        StateStorageService,
+        Principal,
+        CSRFService,
+        AuthServerProvider,
+        UserService,
+        DatePipe
+    ],
+    entryComponents: [JhiLoginModalComponent],
+    exports: [
+        OlddSharedCommonModule,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective,
+        DatePipe
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
+})
+export class OlddSharedModule {}
