780ba32e77e1cac58e43207ba93f05416688b85e
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2019-09-01 09:37:04 -0700

    generate k8s conf

---
+++ kubectl-apply.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Files are ordered in proper order with needed wait for the dependent custom resource definitions to get initialized.
+# Usage: bash kubectl-apply.sh
+
+logSummary(){
+    echo ""
+    echo "#####################################################"
+    echo "Please find the below useful endpoints,"
+    echo "#####################################################"
+}
+
+kubectl apply -f app/
+
+logSummary
