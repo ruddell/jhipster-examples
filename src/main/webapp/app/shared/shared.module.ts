f1181a88a2cdd39dc7a8de5f0ab9f91235911c9a
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2019-11-13 10:58:01 -0800

    basic angular-material setup

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -5,6 +5,7 @@ import { JhiAlertComponent } from './alert/alert.component';
 import { JhiAlertErrorComponent } from './alert/alert-error.component';
 import { JhiLoginModalComponent } from './login/login.component';
 import { HasAnyAuthorityDirective } from './auth/has-any-authority.directive';
+import { JhMaterialModule } from './material.module';
 
 @NgModule({
   imports: [MonoSharedLibsModule],
@@ -16,7 +17,8 @@ import { HasAnyAuthorityDirective } from './auth/has-any-authority.directive';
     JhiAlertComponent,
     JhiAlertErrorComponent,
     JhiLoginModalComponent,
-    HasAnyAuthorityDirective
+    HasAnyAuthorityDirective,
+    JhMaterialModule
   ]
 })
 export class MonoSharedModule {}
