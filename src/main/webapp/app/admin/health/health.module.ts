9f7ec72928c5dcb637f74bea4cd620b7031b547e
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2020-12-01 10:35:24 -0500

    Initial version of mono generated by JHipster-6.10.5

---
+++ src/main/webapp/app/admin/health/health.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MonoSharedModule } from 'app/shared/shared.module';
+
+import { HealthComponent } from './health.component';
+import { HealthModalComponent } from './health-modal.component';
+
+import { healthRoute } from './health.route';
+
+@NgModule({
+  imports: [MonoSharedModule, RouterModule.forChild([healthRoute])],
+  declarations: [HealthComponent, HealthModalComponent],
+  entryComponents: [HealthModalComponent],
+})
+export class HealthModule {}
